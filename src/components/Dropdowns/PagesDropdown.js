3c876e0e930c9dbc5850ec6989bf5e68d13ebe15
عبدالجيد محمد عبدالجيد <40858684+agayed@users.noreply.github.com>
2021-12-19 12:26:03 +0200

    Add files via upload

--- src/components/Dropdowns/PagesDropdown.js
+++ src/components/Dropdowns/PagesDropdown.js
@@ -123,14 +123,6 @@ const PagesDropdown = () => {
         >
           الصفحة الشخصية
         </Link>
-        {/* <Link
-          to="/profile"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-        >
-          أسئلة الزوار
-        </Link> */}
 
       </div>
     </>
