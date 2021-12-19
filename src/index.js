03cf2ff969a075a7a58183627e7c8ae5150df8c9
عبدالجيد محمد عبدالجيد <40858684+agayed@users.noreply.github.com>
2021-12-19 14:43:01 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import "assets/styles/tailwind.css";
 import Admin from "layouts/Admin.js";
 import Auth from "layouts/Auth.js";
 
+// views without layouts
 
 import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
