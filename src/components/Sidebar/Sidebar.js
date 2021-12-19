180e1d085ee63331500836d052c130f16d2894ae
عبدالجيد محمد عبدالجيد <40858684+agayed@users.noreply.github.com>
2021-12-19 12:47:42 +0200

    Add files via upload

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -232,13 +232,6 @@ export default function Sidebar() {
                 </Link>
               </li>
               <li className="items-center">
-                {/* <Link
-                  className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                  to="/Visitors_questions"
-                >
-                  <i className="fas fa-user-circle text-blueGray-400 mr-2 text-sm"></i>{" "}
-                  أسئلة الزوار
-                </Link> */}
               </li>
 
             </ul>
